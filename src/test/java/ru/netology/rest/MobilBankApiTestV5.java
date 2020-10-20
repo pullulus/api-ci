a7396e6c8227973dd29e22923f623afc78d5c54f
andrew-pahomov <59327373+andrew-pahomov@users.noreply.github.com>
2020-10-20 06:25:46 +0300

    Update MobilBankApiTestV5.java

--- src/test/java/ru/netology/rest/MobilBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobilBankApiTestV5.java
@@ -11,7 +11,7 @@ class MobilBankApiTestV5 {
     @Test
     void shouldReturnDemoAccounts() {
         given()
-                .baseUri("http:localhost:9999/api/v1")
+                .baseUri("http://localhost:9999/api/v1")
                 .when()
                 .get("/demo/accounts")
                 .then()
